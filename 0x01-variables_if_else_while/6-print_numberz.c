86d0aad8de020bede886cef931cc5d8192d2c60f
BTISSAME <btissamehlial196@@gmail.com>
2023-03-17 09:49:06 -0700

    print numberz using putchar functions

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - a program that prints all single digit numbers of base 10
+ * you can only use the putchar function
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	int nbr = 36;
+
+	while (nbr <= 57)
+	{
+		putchar(nbr);
+		nbr++;
+	}
+	printf("\n");
+	return (0);
+}
