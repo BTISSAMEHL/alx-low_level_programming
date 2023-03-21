7cbe0187b1c7630685d8cca68b607a1a520c777d
BTISSAME <btissamehlial196@@gmail.com>
2023-03-21 09:33:59 -0700

    104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -5,39 +5,40 @@
  */
 int main(void)
 {
-	int i;
-	unsigned long fib1 = 0, fib2 = 1, sum;
-	unsigned long fib11, fib12, fib21, fib22;
+	int inc;
+	unsigned long n1 = 0, n2 = 1, n3;
+	unsigned long n1_h1, n1_h2, n2_h1, n2_h2;
 	unsigned long h1, h2;
 
-	for (i = 0; i < 92; i++)
+	for (inc = 0; inc < 92; inc++)
 	{
-		sum = fib1 + fib2;
-		printf("%lu, ", sum);
-		fib1 = fib2;
-		fib2 = sum;
+		n3 = n1 + n2;
+		printf("%lu, ", n3);
+		n1 = n2;
+		n2 = n3;
 	}
-	fib11 = fib1 / 10000000000;
-	fib21 = fib2 / 10000000000;
-	fib12 = fib1 % 10000000000;
-	fib22 = fib2 % 10000000000;
-	for (i = 98; i < 99; i++)
+	n1_h1 = n1 / 10000000000;
+	n2_h1 = n2 / 10000000000;
+	n1_h2 = n1 % 10000000000;
+	n2_h2 = n2 % 10000000000;
+	for (inc = 93; inc < 99; inc++)
 	{
-		h1 = fib11 + fib21;
-		h2 = fib12 + fib22;
-		if (fib12 + fib22 > 9999999999)
+		h1 = n1_h1 + n2_h1;
+		h2 = n1_h2 + n2_h2;
+		if ((n1_h2 + n2_h2) > 9999999999)
 		{
 			h1 += 1;
 			h2 %= 10000000000;
 		}
 		printf("%lu%lu", h1, h2);
-		if (i != 98)
+		if (inc != 98)
+		{
 			printf(", ");
-		fib11 = fib21;
-		fib12 = fib22;
-		fib21 = h1;
-		fib22 = h2;
-	}
-	printf("\n");
-	return (0);
+			n1_h1 = n2_h1;
+			n1_h2 = n2_h2;
+			n2_h1 = h1;
+			n2_h2 = h2;
+		}
+		printf("\n");
+		return (0);
 }
