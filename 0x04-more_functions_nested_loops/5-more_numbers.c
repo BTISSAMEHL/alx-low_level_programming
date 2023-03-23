82db4632030def096331817ea631890e4c52fc0f
BTISSAME <btissamehlial196@@gmail.com>
2023-03-23 06:40:22 -0700

    more numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -5,17 +5,16 @@
  */
 void more_numbers(void)
 {
-	int i;
-	int count = 0;
+	int i, j;
 
-	while (count < 10)
+	for (i = 1; i <= 10; i++)
 	{
-		for (i = 0; i <= 14; i++)
+		for (j = 0; j <= 14; j++)
 		{
-			_putchar(i + '0');
-			_putchar('\n');
+			if (j >= 10)
+				_putchar('1');
+			_putchar(j % 10 + '0');
 		}
-		count++;
+		_putchar('\n');
 	}
-	_putchar('\n');
 }
