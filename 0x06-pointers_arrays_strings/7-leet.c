a2a4240ff2e99d972f689b92af7edd06ace41303
BTISSAME <btissamehlial196@@gmail.com>
2023-03-30 10:28:01 -0700

    1337

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,36 @@
+#include "main.h"
+/**
+ * leet - a function that encodes a string into 1337
+ * @a: input
+ * Return: a string into 1337
+ */
+char *leet(char *a)
+{
+	int i = 0;
+
+	while (a[i] != '\0')
+	{
+		if (a[i] == 'a' || a[i] == 'A')
+		{
+			a[i] = '4';
+		}
+		else if (a[i] == 'e' || a[i] == 'E')
+		{
+			a[i] = '3';
+		}
+		else if (a[i] == 'o' || a[i] == 'O')
+		{
+			a[i] = '0';
+		}
+		else if (a[i] == 't' || a[i] == 'T')
+		{
+			a[i] = '7';
+		}
+		else if (a[i] == 'l' || a[i] == 'L')
+		{
+			a[i] = '1';
+		}
+		i++;
+	}
+	return (a);
+}
